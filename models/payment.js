3c2f302677550157197e2cc105d05023f20edf87
Richard Seward <raseward14@gmail.com>
2021-05-25 21:48:00 -0600

    added BSON data type objectid to seeder _id props to seed db and run post, put, delete routes

--- models/payment.js
+++ models/payment.js
@@ -1,23 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-// const Loan = require('./loan');
 
 const paymentSchema = new Schema({
-    _id: {
-        type: Number,
-        primaryKey: false,
-    },
-    balance: {
-        type: Number
-    },
-    date: {
-        type: Date,
-        default: Date.now,
-    },
-    loan_id: {
-        ref: "Loan",
-        type: Schema.Types.ObjectId
-    }
+  balance: {
+    type: Number,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  loan_id: {
+    ref: "Loan",
+    type: Schema.Types.ObjectId,
+  },
 });
 
 const Payment = mongoose.model("Payment", paymentSchema);
