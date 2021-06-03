1a5348b193386987730fa38ce20648d2a632bf3c
b00kbaker <caimmurphy@gmail.com>
2021-06-02 21:11:06 -0600

    Shark Context built out

--- client/src/Context/index.js
+++ client/src/Context/index.js
@@ -1,7 +1,60 @@
-import { useContext, createContext } from "react";
+import React, { useState, createContext } from "react";
+import { useHistory } from "react-router-dom";
 
-export const SharkContext = createContext(null);
+const SharkContext = createContext();
+const { Provider } = SharkContext;
 
-export function useSharkContext() {
-  return useContext(SharkContext);
-}
+const AuthProvider = ({ children }) => {
+  const history = useHistory();
+  const token = localStorage.getItem("token");
+  const userInfo = localStorage.getItem("userInfo");
+  const expiresAt = localStorage.getItem("expiersAt");
+
+  const [authState, setAuthState] = useState({
+    token,
+    expiresAt,
+    userInfo: userInfo ? JSON.parse(userInfo) : {},
+  });
+
+  const setAuthInfo = ({ token, userInfo, expiresAt }) => {
+    localStorage.setItem("token", token);
+    localStorage.setItem("userInfo", JSON.stringify(userInfo));
+    localStorage.setItem("expiresAt", expiresAt);
+
+    setAuthState({
+      token,
+      userInfo,
+      expiresAt,
+    });
+  };
+
+  const signout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("userInfo");
+    localStorage.removeItem("expiresAt");
+    setAuthState({});
+    history.push("/signin");
+  };
+
+  const isAuthenticated = () => {
+    if (!authState.token || !authState.expiresAt) {
+      return false;
+    }
+    return new Date().getTime() / 1000 < authState.expiresAt;
+  };
+
+  return(
+    <Provider
+    value={{
+      authState,
+      setAuthState: (authInfo) => setAuthInfo(authInfo),
+      logout,
+      isAuthenticated,
+    }}
+    >
+      {children}
+    </Provider>
+  );
+};
+
+export { SharkContext, AuthProvider };
