24bbabbd54ba21c8aa29c788c697d7ff0fe32b52
Richard Seward <raseward14@gmail.com>
2021-06-12 17:14:35 -0600

    logout button redirects to homepage

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -3,58 +3,16 @@ import Jumbo from "../components/Jumbotron/index";
 // import Introduction from "../components/Introduction/index";
 import WhatIsLoanShark from "../components/WhatIsLoanShark/index";
 
-// import API from "../utils/API";
-// import { List, ListItem } from "../components/List";
-
-// THIS WAS PART OF WHAT I COMMENTED OUT -----------
-// import
-//     // Input,
-//     // TextArea,
-//     FormBtn from "../components/Form";
-
 function Homepage() {
-  // hooks
-  // const [loans, setLoans] = useState([])
-  // const [formObj, setFormObj] = useState([])
-
-  // useEffect(() => {
-  // loadLoans()
-  // }, [])
-
-  // function loadLoans() {
-  // API.getLoans()
-  // }
-
-  // function deleteLoans() {
-  // API.deleteLoans()
-  // }
-
-  // function handleInputChange() {
-  // setFormObj
-  // }
-
-  // function handleFormSubmit() {
-  // event.prevent.default
-  // API.saveLoan()
-  // }
 
   return (
     <div>
       <Jumbo>
         <h1>Your Loans Live Here.</h1>
       </Jumbo>
-      {/* <Introduction /> */}
       <WhatIsLoanShark />
     </div>
   );
 }
 
 export default Homepage;
-
-// jumbotron
-
-// introduction
-
-// signin button
-
-// signup button
